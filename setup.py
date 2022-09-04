cd84cbbe87e275546390cf74eb67abb84803e796
Alexander Yuryatin <u_alal@yahoo.com>
2022-09-04 03:17:19 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     packages = ['salesplansuccess'],
     version = '0.0.1',
     description = 'This package helps estimate the probability of achieving the annual sales plan for a specific product based on historical monthly sales data with the help of ARIMA modeling and Monte Carlo simulation',
-    author = 'Alexander Yuryatin',
-    author_email = '',
     url = 'https://github.com/yuryatin/salesplansuccess',
     download_url = 'https://github.com/yuryatin/salesplansuccess/archive/refs/tags/v0.0.1.tar.gz',
     keywords = ['sales', 'forecast', 'arima', 'simulation', 'pharma', 'medications', 'pharmacy', 'pharmaceutical', 'ics', 'offtake'],
