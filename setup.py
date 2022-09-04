d348e218d0170c1ad6ea7b21fd2424f4f90734da
Alexander Yuryatin <u_alal@yahoo.com>
2022-09-04 15:29:02 +0300

    updated version number in tar link

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license = 'MIT',
     description = 'This package helps estimate the probability of achieving the annual sales plan for a specific product based on historical monthly sales data with the help of ARIMA modeling and Monte Carlo simulation',
     url = 'https://github.com/yuryatin/salesplansuccess',
-    download_url = 'https://github.com/yuryatin/salesplansuccess/archive/refs/tags/v0.0.2.tar.gz',
+    download_url = 'https://github.com/yuryatin/salesplansuccess/archive/refs/tags/v0.0.3.tar.gz',
     keywords = ['sales', 'forecast', 'arima', 'simulation', 'pharma', 'medications', 'pharmacy', 'pharmaceutical', 'ics', 'offtake'],
     classifiers = [],
     install_requires = ['numpy','pandas','matplotlib','statsmodels', 'scipy', 'datetime']
