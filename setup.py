1ef8d79946e2dcd312d02033698b684967faafc8
Alexander Yuryatin <u_alal@yahoo.com>
2022-09-04 04:21:52 +0300

    updated version number

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'salesplansuccess',
     packages = ['salesplansuccess'],
-    version = '0.0.1',
+    version = '0.0.2',
     license = 'MIT',
     description = 'This package helps estimate the probability of achieving the annual sales plan for a specific product based on historical monthly sales data with the help of ARIMA modeling and Monte Carlo simulation',
     url = 'https://github.com/yuryatin/salesplansuccess',
-    download_url = 'https://github.com/yuryatin/salesplansuccess/archive/refs/tags/v0.0.1.tar.gz',
+    download_url = 'https://github.com/yuryatin/salesplansuccess/archive/refs/tags/v0.0.2.tar.gz',
     keywords = ['sales', 'forecast', 'arima', 'simulation', 'pharma', 'medications', 'pharmacy', 'pharmaceutical', 'ics', 'offtake'],
     classifiers = [],
     install_requires = ['numpy','pandas','matplotlib','statsmodels', 'scipy', 'datetime']
